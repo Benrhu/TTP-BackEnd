7af8f1ea3fa95155b78f61055868d923dc101580
Benrhu <rubendiaz300000@gmail.com>
2022-06-17 18:31:45 +0200

    fix(vaidate-object-id-pipes)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,49 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MoneyModule } from './wallet/wallet.module';
+import { WalletModule } from './wallet/wallet.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { WalletSchema } from './wallet/schema/wallet.schema';
+import { User, UserSchema } from './users/schema/user.schema';
+import { WalletService } from './wallet/wallet.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
     UsersModule,
-    MoneyModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    WalletModule,
+    MongooseModule.forFeatureAsync([
+      {
+        name: 'Wallet',
+        useFactory: () => {
+          const schema = WalletSchema;
+          schema.pre('Save', function () {
+            console.log('Pre save');
+          });
+          return schema;
+        },
+      },
+    ]),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest', {
+      connectionName: 'wallet',
+    }),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.pre('Save', function () {
+            console.log('Pre save');
+          });
+          return schema;
+        },
+      },
+    ]),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest', {
+      connectionName: 'user',
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WalletService, UsersService],
 })
 export class AppModule {}
